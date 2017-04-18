68343d1c34dde390b9dc0a934b3cdba51fab5459
andreffonseca <andre.fg.fonseca@gmail.com>
2017-04-18 23:08:05 +0100

    Dev

---
+++ src/app/DataModels/ApiCallModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataModels;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Monitor API Database Model
+ * 
+ * This class is responsible to handle all the database activity
+ * with the table <stong>xx</strong>    
+ * 
+ * @package Monitor
+ * @category Data Models    
+ * @author André Fonseca <andre.fg.fonseca@gmail.com>
+ * @version 1.0.a
+ * @since 1.0.a
+ */
+class ApiCallModel extends Model
+{
+    /**
+     * Class Table name
+     * @var string 
+     */
+    protected $table = '';
+}
---
+++ src/app/Http/Controllers/ApiCallController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+/**
+ * Monitor API Class Controller
+ * 
+ * This class will handle all the requests made to the entry API.
+ * 
+ * @package Monitor
+ * @category Controllers    
+ * @author André Fonseca <andre.fg.fonseca@gmail.com>
+ * @version 1.0.a
+ * @since 1.0.a
+ * 
+ */
+class ApiCallController extends Controller
+{
+    
+    /**
+     * API Request handler - POST
+     * 
+     * This method will handle all the requests made to the MONITOR API
+     * using POST HTTP method
+     * 
+     * 
+     * @author André Fonseca <andre.fg.fonseca@gmail.com>
+     * @since 1.0.0
+     * 
+     * @param Request $request
+     * @return void
+     * 
+     */
+    public function postAPIRequest(Request $request) {
+        
+    }
+    
+    
+    
+}
